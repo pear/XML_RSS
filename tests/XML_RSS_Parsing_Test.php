5a930b3623d06b581a867511e0e27f48546a8547
Clay Loveless <clay@php.net>
2006-09-14 07:29:41 +0000

    Added additional unit tests for public methods.
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_RSS/trunk@219944 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/XML_RSS_Parsing_Test.php
+++ tests/XML_RSS_Parsing_Test.php
@@ -48,6 +48,96 @@ class XML_RSS_Parsing_Test extends PHPUnit_TestCase {
             $i++;
         }
     }
+
+    function testGetStructure()
+    {
+        $r =& new XML_RSS('test.rss');
+        $r->parse();
+
+        $expected = 'a:7:{i:0;a:4:{s:4:"type";s:7:"channel";s:5:"title";s:4:"Test";s:4:"link";s:20:"http://pear.php.net/";s:11:"description";s:34:"This is a test channel for XML_RSS";}i:1;a:4:{s:4:"type";s:5:"image";s:5:"title";s:4:"PEAR";s:3:"url";s:38:"http://pear.php.net/gifs/pearsmall.gif";s:4:"link";s:20:"http://pear.php.net/";}i:2;a:3:{s:4:"type";s:4:"item";s:5:"title";s:12:"PHP homepage";s:4:"link";s:15:"http://php.net/";}i:3;a:3:{s:4:"type";s:4:"item";s:5:"title";s:13:"PEAR homepage";s:4:"link";s:20:"http://pear.php.net/";}i:4;a:3:{s:4:"type";s:4:"item";s:5:"title";s:16:"PHP-GTK homepage";s:4:"link";s:19:"http://gtk.php.net/";}i:5;a:3:{s:4:"type";s:4:"item";s:5:"title";s:15:"PHP QA homepage";s:4:"link";s:18:"http://qa.php.net/";}i:6;a:5:{s:4:"type";s:9:"textinput";s:5:"title";s:15:"Search Slashdot";s:11:"description";s:23:"Search Slashdot stories";s:4:"name";s:5:"query";s:4:"link";s:29:"http://slashdot.org/search.pl";}}';
+        $actual = serialize($r->getStructure());
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    function testGetStructureFromString()
+    {
+        $rss = file_get_contents('test.rss');
+        $r =& new XML_RSS($rss);
+        $r->parse();
+
+        $expected = 'a:7:{i:0;a:4:{s:4:"type";s:7:"channel";s:5:"title";s:4:"Test";s:4:"link";s:20:"http://pear.php.net/";s:11:"description";s:34:"This is a test channel for XML_RSS";}i:1;a:4:{s:4:"type";s:5:"image";s:5:"title";s:4:"PEAR";s:3:"url";s:38:"http://pear.php.net/gifs/pearsmall.gif";s:4:"link";s:20:"http://pear.php.net/";}i:2;a:3:{s:4:"type";s:4:"item";s:5:"title";s:12:"PHP homepage";s:4:"link";s:15:"http://php.net/";}i:3;a:3:{s:4:"type";s:4:"item";s:5:"title";s:13:"PEAR homepage";s:4:"link";s:20:"http://pear.php.net/";}i:4;a:3:{s:4:"type";s:4:"item";s:5:"title";s:16:"PHP-GTK homepage";s:4:"link";s:19:"http://gtk.php.net/";}i:5;a:3:{s:4:"type";s:4:"item";s:5:"title";s:15:"PHP QA homepage";s:4:"link";s:18:"http://qa.php.net/";}i:6;a:5:{s:4:"type";s:9:"textinput";s:5:"title";s:15:"Search Slashdot";s:11:"description";s:23:"Search Slashdot stories";s:4:"name";s:5:"query";s:4:"link";s:29:"http://slashdot.org/search.pl";}}';
+        $actual = serialize($r->getStructure());
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    function testGetChannelInfo()
+    {
+        $r =& new XML_RSS('test.rss');
+        $r->parse();
+
+        $expected = array(
+            'title'         => 'Test',
+            'link'          => 'http://pear.php.net/',
+            'description'   => 'This is a test channel for XML_RSS'
+        );
+
+        $actual = $r->getChannelInfo();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    function testGetItems()
+    {
+        $r =& new XML_RSS('test.rss');
+        $r->parse();
+
+        $expected = array();
+        $expected[] = array('title' => 'PHP homepage', 'link' => 'http://php.net/');
+        $expected[] = array('title' => 'PEAR homepage', 'link' => 'http://pear.php.net/');
+        $expected[] = array('title' => 'PHP-GTK homepage', 'link' => 'http://gtk.php.net/');
+        $expected[] = array('title' => 'PHP QA homepage', 'link' => 'http://qa.php.net/');
+
+        $actual = $r->getItems();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    function testGetImages()
+    {
+        $r =& new XML_RSS('test.rss');
+        $r->parse();
+
+        $expected = array();
+        $expected[] = array(
+            'title' => 'PEAR',
+            'url'   => 'http://pear.php.net/gifs/pearsmall.gif',
+            'link'  => 'http://pear.php.net/'
+        );
+
+        $actual = $r->getImages();
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    function testGetTextinputs()
+    {
+        $r =& new XML_RSS('test.rss');
+        $r->parse();
+
+        $expected = array(
+            'title'         => 'Search Slashdot',
+            'description'   => 'Search Slashdot stories',
+            'name'          => 'query',
+            'link'          => 'http://slashdot.org/search.pl'
+        );
+
+        $actual = $r->getTextinputs();
+
+        $this->assertEquals($expected, $actual);
+    }
+
 }
 
 $suite = new PHPUnit_TestSuite("XML_RSS_Parsing_Test");
