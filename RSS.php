d9b20ae4f160edfcd59187fbd52e3d25cdc37a66
Christian Weiske <cweiske@php.net>
2010-02-14 14:52:37 +0000

    Fix CS and fix Bug #17007: Wrong PHP license version
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_RSS/trunk@295064 c90b9560-bf6c-de11-be94-00142212c4b1

--- RSS.php
+++ RSS.php
@@ -1,38 +1,34 @@
 <?php
 // vim: set expandtab tabstop=4 shiftwidth=4 fdm=marker:
-// +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2003 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.02 of the PHP license,      |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Martin Jansen <mj@php.net>                                  |
-// |                                                                      |
-// +----------------------------------------------------------------------+
-//
-// $Id$
-//
+
+/**
+ * XML RSS parser
+ *
+ * PHP version 4
+ *
+ * @category XML
+ * @package  XML_RSS
+ * @author   Martin Jansen <mj@php.net>
+ * @license  PHP License http://php.net/license
+ * @version  SVN: $Id$
+ * @link     http://pear.php.net/package/XML_RSS
+ */
 
 require_once 'XML/Parser.php';
 
 /**
-* RSS parser class.
-*
-* This class is a parser for Resource Description Framework (RDF) Site
-* Summary (RSS) documents. For more information on RSS see the
-* website of the RSS working group (http://www.purl.org/rss/).
-*
-* @author Martin Jansen <mj@php.net>
-* @version $Revision$
-* @access  public
-*/
+ * RSS parser class.
+ *
+ * This class is a parser for Resource Description Framework (RDF) Site
+ * Summary (RSS) documents. For more information on RSS see the
+ * website of the RSS working group (http://www.purl.org/rss/).
+ *
+ * @category XML
+ * @package  XML_RSS
+ * @author   Martin Jansen <mj@php.net>
+ * @license  PHP License http://php.net/license
+ * @link     http://pear.php.net/package/XML_RSS
+ */
 class XML_RSS extends XML_Parser
 {
     // {{{ properties
@@ -95,18 +91,22 @@ class XML_RSS extends XML_Parser
     /**
      * @var array
      */
-    var $channelTags = array('TITLE', 'LINK', 'DESCRIPTION', 'IMAGE',
-                              'ITEMS', 'TEXTINPUT', 'LANGUAGE', 'COPYRIGHT',
-                              'MANAGINGEditor', 'WEBMASTER', 'PUBDATE', 'LASTBUILDDATE',
-                              'CATEGORY', 'GENERATOR', 'DOCS', 'CLOUD', 'TTL',
-                              'RATING');
+    var $channelTags = array(
+        'TITLE', 'LINK', 'DESCRIPTION', 'IMAGE',
+        'ITEMS', 'TEXTINPUT', 'LANGUAGE', 'COPYRIGHT',
+        'MANAGINGEditor', 'WEBMASTER', 'PUBDATE', 'LASTBUILDDATE',
+        'CATEGORY', 'GENERATOR', 'DOCS', 'CLOUD', 'TTL',
+        'RATING'
+    );
 
     /**
      * @var array
      */
-    var $itemTags = array('TITLE', 'LINK', 'DESCRIPTION', 'PUBDATE', 'AUTHOR', 'CATEGORY',
-                          'COMMENTS', 'ENCLOSURE', 'GUID', 'SOURCE',
-                          'CONTENT:ENCODED');
+    var $itemTags = array(
+        'TITLE', 'LINK', 'DESCRIPTION', 'PUBDATE', 'AUTHOR', 'CATEGORY',
+        'COMMENTS', 'ENCLOSURE', 'GUID', 'SOURCE',
+        'CONTENT:ENCODED'
+    );
 
     /**
      * @var array
@@ -133,25 +133,26 @@ class XML_RSS extends XML_Parser
      *
      * @var array
      */
-    var $moduleTags = array('DC:TITLE', 'DC:CREATOR', 'DC:SUBJECT', 'DC:DESCRIPTION',
-                            'DC:PUBLISHER', 'DC:CONTRIBUTOR', 'DC:DATE', 'DC:TYPE',
-                            'DC:FORMAT', 'DC:IDENTIFIER', 'DC:SOURCE', 'DC:LANGUAGE',
-                            'DC:RELATION', 'DC:COVERAGE', 'DC:RIGHTS',
-                            'BLOGCHANNEL:BLOGROLL', 'BLOGCHANNEL:MYSUBSCRIPTIONS',
-                            'BLOGCHANNEL:BLINK', 'BLOGCHANNEL:CHANGES',
-                            'CREATIVECOMMONS:LICENSE', 'CC:LICENSE', 'CONTENT:ENCODED',
-                            'SY:UPDATEPERIOD', 'SY:UPDATEFREQUENCY', 'SY:UPDATEBASE',
-                            'TRACKBACK:PING', 'GEO:LAT', 'GEO:LONG',
-                            'MEDIA:GROUP', 'MEDIA:CONTENT', 'MEDIA:ADULT',
-                            'MEDIA:RATING', 'MEDIA:TITLE', 'MEDIA:DESCRIPTION',
-                            'MEDIA:KEYWORDS', 'MEDIA:THUMBNAIL', 'MEDIA:CATEGORY',
-                            'MEDIA:HASH', 'MEDIA:PLAYER', 'MEDIA:CREDIT',
-                            'MEDIA:COPYRIGHT', 'MEDIA:TEXT', 'MEDIA:RESTRICTION',
-                            'ITUNES:AUTHOR', 'ITUNES:BLOCK', 'ITUNES:CATEGORY',
-                            'ITUNES:DURATION', 'ITUNES:EXPLICIT', 'ITUNES:IMAGE',
-                            'ITUNES:KEYWORDS', 'ITUNES:NEW-FEED-URL', 'ITUNES:OWNER',
-                            'ITUNES:PUBDATE', 'ITUNES:SUBTITLE', 'ITUNES:SUMMARY'
-                            );
+    var $moduleTags = array(
+        'DC:TITLE', 'DC:CREATOR', 'DC:SUBJECT', 'DC:DESCRIPTION',
+        'DC:PUBLISHER', 'DC:CONTRIBUTOR', 'DC:DATE', 'DC:TYPE',
+        'DC:FORMAT', 'DC:IDENTIFIER', 'DC:SOURCE', 'DC:LANGUAGE',
+        'DC:RELATION', 'DC:COVERAGE', 'DC:RIGHTS',
+        'BLOGCHANNEL:BLOGROLL', 'BLOGCHANNEL:MYSUBSCRIPTIONS',
+        'BLOGCHANNEL:BLINK', 'BLOGCHANNEL:CHANGES',
+        'CREATIVECOMMONS:LICENSE', 'CC:LICENSE', 'CONTENT:ENCODED',
+        'SY:UPDATEPERIOD', 'SY:UPDATEFREQUENCY', 'SY:UPDATEBASE',
+        'TRACKBACK:PING', 'GEO:LAT', 'GEO:LONG',
+        'MEDIA:GROUP', 'MEDIA:CONTENT', 'MEDIA:ADULT',
+        'MEDIA:RATING', 'MEDIA:TITLE', 'MEDIA:DESCRIPTION',
+        'MEDIA:KEYWORDS', 'MEDIA:THUMBNAIL', 'MEDIA:CATEGORY',
+        'MEDIA:HASH', 'MEDIA:PLAYER', 'MEDIA:CREDIT',
+        'MEDIA:COPYRIGHT', 'MEDIA:TEXT', 'MEDIA:RESTRICTION',
+        'ITUNES:AUTHOR', 'ITUNES:BLOCK', 'ITUNES:CATEGORY',
+        'ITUNES:DURATION', 'ITUNES:EXPLICIT', 'ITUNES:IMAGE',
+        'ITUNES:KEYWORDS', 'ITUNES:NEW-FEED-URL', 'ITUNES:OWNER',
+        'ITUNES:PUBDATE', 'ITUNES:SUBTITLE', 'ITUNES:SUMMARY'
+    );
 
     /**
      * @var array
@@ -164,13 +165,14 @@ class XML_RSS extends XML_Parser
     /**
      * Constructor
      *
-     * @access public
-     * @param mixed File pointer, name of the RSS file, or an RSS string.
-     * @param string  Source charset encoding, use null (default) to use
-     *                default encoding (ISO-8859-1)
-     * @param string  Target charset encoding, use null (default) to use
-     *                default encoding (ISO-8859-1)
+     * @param mixed  $handle File pointer, name of the RSS file, or an RSS string.
+     * @param string $srcenc Source charset encoding, use null (default)
+     *                       to use default encoding (ISO-8859-1)
+     * @param string $tgtenc Target charset encoding, use null (default)
+     *                       to use default encoding (ISO-8859-1)
+     *
      * @return void
+     * @access public
      */
     function XML_RSS($handle = '', $srcenc = null, $tgtenc = null)
     {
@@ -193,11 +195,12 @@ class XML_RSS extends XML_Parser
     /**
      * Start element handler for XML parser
      *
-     * @access private
-     * @param  object XML parser object
-     * @param  string XML element
-     * @param  array  Attributes of XML tag
+     * @param object $parser  XML parser object
+     * @param string $element XML element
+     * @param array  $attribs Attributes of XML tag
+     *
      * @return void
+     * @access private
      */
     function startHandler($parser, $element, $attribs)
     {
@@ -206,20 +209,20 @@ class XML_RSS extends XML_Parser
         }
 
         switch ($element) {
-            case 'CHANNEL':
-            case 'ITEM':
-            case 'IMAGE':
-            case 'TEXTINPUT':
-                $this->insideTag = $element;
-                array_push($this->insideTagStack, $element);
-                break;
-
-            case 'ENCLOSURE' :
-                $this->attribs = $attribs;
-                break;
-
-            default:
-                $this->activeTag = $element;
+        case 'CHANNEL':
+        case 'ITEM':
+        case 'IMAGE':
+        case 'TEXTINPUT':
+            $this->insideTag = $element;
+            array_push($this->insideTagStack, $element);
+            break;
+        
+        case 'ENCLOSURE' :
+            $this->attribs = $attribs;
+            break;
+            
+        default:
+            $this->activeTag = $element;
         }
     }
 
@@ -234,10 +237,11 @@ class XML_RSS extends XML_Parser
      * $this->struct[] and adds the field "type" to this array,
      * that defines the type of the current field.
      *
-     * @access private
-     * @param  object XML parser object
-     * @param  string
+     * @param object $parser  XML parser object
+     * @param string $element Name of element that ends
+     *
      * @return void
+     * @access private
      */
     function endHandler($parser, $element)
     {
@@ -249,8 +253,10 @@ class XML_RSS extends XML_Parser
             array_pop($this->insideTagStack);
             $this->insideTag = end($this->insideTagStack);
 
-            $this->struct[] = array_merge(array('type' => strtolower($element)),
-                                          $this->last);
+            $this->struct[] = array_merge(
+                array('type' => strtolower($element)),
+                $this->last
+            );
         }
 
         if ($element == 'ITEM') {
@@ -273,7 +279,9 @@ class XML_RSS extends XML_Parser
                 $this->item['enclosures'] = array();
             }
 
-            $this->item['enclosures'][] = array_change_key_case($this->attribs, CASE_LOWER);
+            $this->item['enclosures'][] = array_change_key_case(
+                $this->attribs, CASE_LOWER
+            );
             $this->attribs = array();
         }
 
@@ -286,10 +294,11 @@ class XML_RSS extends XML_Parser
     /**
      * Handler for character data
      *
-     * @access private
-     * @param  object XML parser object
-     * @param  string CDATA
+     * @param object $parser XML parser object
+     * @param string $cdata  CDATA
+     *
      * @return void
+     * @access private
      */
     function cdataHandler($parser, $cdata)
     {
@@ -297,10 +306,13 @@ class XML_RSS extends XML_Parser
             $tagName = strtolower($this->insideTag);
             $var = $this->{$tagName . 'Tags'};
 
-            if (in_array($this->activeTag, $var) ||
-                in_array($this->activeTag, $this->moduleTags)) {
-                $this->_add($tagName, strtolower($this->activeTag),
-                            $cdata);
+            if (in_array($this->activeTag, $var)
+                || in_array($this->activeTag, $this->moduleTags)
+            ) {
+                $this->_add(
+                    $tagName, strtolower($this->activeTag),
+                    $cdata
+                );
             }
 
         }
@@ -312,10 +324,11 @@ class XML_RSS extends XML_Parser
     /**
      * Default handler for XML parser
      *
-     * @access private
-     * @param  object XML parser object
-     * @param  string CDATA
+     * @param object $parser XML parser object
+     * @param string $cdata  CDATA
+     *
      * @return void
+     * @access private
      */
     function defaultHandler($parser, $cdata)
     {
@@ -328,11 +341,12 @@ class XML_RSS extends XML_Parser
     /**
      * Add element to internal result sets
      *
-     * @access private
-     * @param  string Name of the result set
-     * @param  string Fieldname
-     * @param  string Value
+     * @param string $type  Name of the result set
+     * @param string $field Fieldname
+     * @param string $value Value
+     *
      * @return void
+     * @access private
      * @see    cdataHandler
      */
     function _add($type, $field, $value)
@@ -352,8 +366,8 @@ class XML_RSS extends XML_Parser
     /**
      * Get complete structure of RSS file
      *
-     * @access public
      * @return array
+     * @access public
      */
     function getStructure()
     {
@@ -370,8 +384,8 @@ class XML_RSS extends XML_Parser
      * that has been extracted from the <channel>-tag while parsing
      * the RSS file.
      *
-     * @access public
      * @return array
+     * @access public
      */
     function getChannelInfo()
     {
@@ -387,8 +401,8 @@ class XML_RSS extends XML_Parser
      * This method returns an array containing the set of items
      * that are provided by the RSS file.
      *
-     * @access public
      * @return array
+     * @access public
      */
     function getItems()
     {
@@ -404,8 +418,8 @@ class XML_RSS extends XML_Parser
      * This method returns an array containing the set of images
      * that are provided by the RSS file.
      *
-     * @access public
      * @return array
+     * @access public
      */
     function getImages()
     {
@@ -418,8 +432,8 @@ class XML_RSS extends XML_Parser
     /**
      * Get text input fields from RSS file
      *
-     * @access public
      * @return array
+     * @access public
      */
     function getTextinputs()
     {
